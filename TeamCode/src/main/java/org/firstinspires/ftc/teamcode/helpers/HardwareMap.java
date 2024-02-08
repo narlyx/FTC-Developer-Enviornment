fc4e39733be2e9ded552493f82be88debb322883
Chester <109835029+narlyx@users.noreply.github.com>
2024-02-08 15:21:34 -0700

    Setup HardwareMap and improved template opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/HardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/HardwareMap.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.helpers;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.chesterlk.ftc.tweetybird.TweetyBirdProcessor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * The purpose of this class is to globally provide hardware configuration + other variables
@@ -12,7 +14,6 @@ public class HardwareMap {
      * Opmode Reference
      */
     private LinearOpMode opMode;
-    private com.qualcomm.robotcore.hardware.HardwareMap hwMap;
 
     /**
      * TweetyBird Reference
@@ -25,20 +26,59 @@ public class HardwareMap {
     public FtcDashboard ftcDashboard = FtcDashboard.getInstance();
 
     /**
+     * Defining Hardware
+     */
+    //Motors
+    public DcMotor FL,FR,BL,BR;
+
+    //Encoders
+    public DcMotor leftEncoder,rightEncoder,middleEncoder;
+
+
+    /**
      * Constructor
      */
     public HardwareMap(LinearOpMode importedOpmode) {
         //Pull OpMode
         opMode = importedOpmode;
-
-        //Setting HardwareMap
-        hwMap = importedOpmode.hardwareMap;
     }
 
     /**
      * Init
      */
-    public void init() {
+    public void initGeneral() {
+        com.qualcomm.robotcore.hardware.HardwareMap hwMap = opMode.hardwareMap;
 
+        //Motors
+        FL = hwMap.get(DcMotor.class, "FL");
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FL.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        FR = hwMap.get(DcMotor.class, "FR");
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        BL = hwMap.get(DcMotor.class, "BL");
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BL.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        BR = hwMap.get(DcMotor.class, "BR");
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //Encoders
+        leftEncoder = hwMap.get(DcMotor.class, "FL");
+        leftEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftEncoder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        rightEncoder = hwMap.get(DcMotor.class, "BL");
+        rightEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightEncoder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        middleEncoder = hwMap.get(DcMotor.class, "BR");
+        middleEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        middleEncoder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/TemplateAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/TemplateAuto.java
@@ -1,13 +1,29 @@
 package org.firstinspires.ftc.teamcode.helpers;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name = "Change Me", group = "a")
+@Disabled //Do not forget to remove this line to make it active
 public class TemplateAuto extends LinearOpMode {
 
+    /**
+     * HardwareMap Reference
+     */
+    private final HardwareMap robot = new HardwareMap(this);
+
+    /**
+     * Opmode Start
+     */
     @Override
     public void runOpMode() {
+        //Initialize
+        robot.initGeneral();
+
+        waitForStart();
+        //Run
+
 
     }
 }
